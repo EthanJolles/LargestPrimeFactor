ccfdb6090c2dc4074d497741ea6498abc51d5893
Ethan Jolles <85588248+EthanJolles@users.noreply.github.com>
2021-09-22 02:39:43 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int x)
+    {
+        if(x<0)
+        {
+            return -1;
+        }
+
+        for(int i = x; i>0; i--)
+        {
+            if(x%i==0 && isPrime(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isPrime(int n)
+    {
+        if(n==1)
+        {
+            return false;
+        }
+        for(int i=2; i <= n/2; i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
